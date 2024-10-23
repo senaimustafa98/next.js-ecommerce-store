f93de50060d553a5b5db96a35c955f8c38163a8f
Senay Mustafa <senaim98@gmail.com>
2024-10-23 19:03:31 +0200

    Fix playwright testing

--- playwright/addToCart.test.js
+++ playwright/addToCart.test.js
@@ -7,16 +7,12 @@ test('Add to cart', async ({ page }) => {
   // Click the add to cart button
   await page.click('[data-test-id="product-add-to-cart"]');
 
-  // Optional: Wait a bit to ensure the cart updates
   await page.waitForTimeout(500); // Wait for 0.5 seconds for the cart to update
-
-  // Now navigate to the cart page
   await page.goto('http://localhost:3000/cart');
 
-  // Wait for the cart item to be visible
   await page.waitForSelector('h2:has-text("Fur Brush")');
   const cartItem = page.locator('h2:has-text("Fur Brush")');
 
-  // Assert that the cart item is visible
+
   await expect(cartItem).toBeVisible();
 });
--- playwright/cartQuantity.test.js
+++ playwright/cartQuantity.test.js
@@ -12,20 +12,15 @@ test('Change quantity of item in cart', async ({ page }) => {
   const quantityInput = page.locator('[data-test-id="product-quantity"]');
   await quantityInput.fill('2');
 
-  // Confirm the button is visible before clicking
   const addToCartButton = page.locator('[data-test-id="product-add-to-cart"]');
   await expect(addToCartButton).toBeVisible();
-
-  // Click the add to cart button
   await addToCartButton.click();
 
-  // Optional: Wait a bit to ensure the cart updates
   await page.waitForTimeout(500); // Wait for 0.5 seconds for the cart to update
 
 
   await page.goto('http://localhost:3000/cart');
 
-  // Verify that the quantity input now has a value of 2
   const cartQuantityInput = page.locator('[data-test-id="cart-product-quantity-1"]');
   await expect(cartQuantityInput).toHaveValue('2');
 });
