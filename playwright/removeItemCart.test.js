22f539b27eae80975837b594cf93059c3baed284
Senay Mustafa <senaim98@gmail.com>
2024-10-23 19:26:44 +0200

    Change removeItems test logic

--- playwright/removeItemCart.test.js
+++ playwright/removeItemCart.test.js
@@ -1,14 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('Remove item from cart', async ({ page }) => {
-  // Go to the cart page directly
   await page.goto('http://localhost:3000/cart');
-
-  await page.waitForSelector('[data-test-id="cart-product-1"]');
 
 
   const removeButton = page.locator('[data-test-id="cart-product-remove-1"]');
   await removeButton.click();
+
 
   const emptyCartMessage = page.locator('text=Cart is empty');
   await expect(emptyCartMessage).toBeVisible();
